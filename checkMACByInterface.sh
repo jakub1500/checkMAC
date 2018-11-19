db176eb350d58116cdbf246dc339ad4124a91d31
jakub <jakub1500@gmail.com>
2018-11-19 20:50:13 +0100

    Refactor of checkMACByInterface.sh

--- checkMACByInterface.sh
+++ checkMACByInterface.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-MAC=$(ifconfig $1 | grep ether | grep -Eo "([a-fA-F0-9]{2}\:){5}[a-fA-F0-9]{2}")
-MAC_CUTTED=$(echo $MAC | cut -c -8)
-VENDOR=$(cat $HOME/repos/checkMAC/MAC_VS_VENDOR | grep $MAC_CUTTED | cut -c 10-)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+MAC=$(ifconfig "$1" | grep ether | grep -Eo "([a-fA-F0-9]{2}\:){5}[a-fA-F0-9]{2}")
+MAC_CUTTED=$(echo "$MAC" | cut -c -8)
+VENDOR=$(grep "$MAC_CUTTED" "$SCRIPT_DIR/MAC_VS_VENDOR" | cut -c 10-)
 echo "$MAC"
 echo "$VENDOR"
